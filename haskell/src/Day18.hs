5b6d3d8caa1a9a651cdc25fa5a670a1ab1feb983
Johan Levin <johan13@gmail.com>
2024-12-19 22:49:51 +0100

    Refactor day 18

--- haskell/src/Day18.hs
+++ haskell/src/Day18.hs
@@ -3,7 +3,7 @@ import Data.Function (on)
 import Data.Heap (MinHeap)
 import qualified Data.Heap as H
 import Data.List.Split (splitOn)
-import Data.Maybe (fromMaybe, isJust)
+import Data.Maybe (fromMaybe, isNothing)
 import qualified Data.Set as S
 
 day18p1 :: String -> Int
@@ -13,37 +13,36 @@ day18p2 :: String -> String
 day18p2 = (\(x, y) -> show x ++ "," ++ show y) . firstBlockingByte . parseInput
 
 type Pos = (Int, Int)
-data Entry = Entry Pos Int deriving (Eq)
-type Queue = MinHeap Entry
+data QueueEntry = QueueEntry Pos Int deriving (Eq)
 
-instance Ord Entry where
-    compare = compare `on` (\(Entry (x, y) steps) -> steps - x - y) -- Sort for A* algorithm
+instance Ord QueueEntry where
+    compare = compare `on` (\(QueueEntry (x, y) steps) -> steps - x - y) -- Sort for A* algorithm.
 
 -- Get the length of the shortest path (if one exists) from 0,0 to 70,70 given a list of corrupt bytes.
 shortestPath :: [Pos] -> Maybe Int
-shortestPath allBytes = shortestPath' (S.fromList allBytes) initialQueue S.empty
+shortestPath = shortestPath' initialQueue . S.fromList
   where
-    initialQueue = H.singleton (Entry (0, 0) 0) :: Queue
-    neighbors (x, y) = [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
-    shortestPath' bytes queue visited = case H.view queue of
+    initialQueue = H.singleton (QueueEntry (0, 0) 0) :: MinHeap QueueEntry
+    shortestPath' queue avoid = case H.view queue of
         Nothing -> Nothing
-        Just (Entry (70, 70) steps, _) -> Just steps
-        Just (Entry pos steps, tailQueue) -> shortestPath' bytes queue' visited'
+        Just (QueueEntry (70, 70) steps, _) -> Just steps
+        Just (QueueEntry (x, y) steps, tailQueue) -> shortestPath' queue' avoid'
           where
-            visited' = S.insert pos visited
-            queue' = foldl' enqueue tailQueue $ filter shouldVisit $ neighbors pos
-            enqueue q (x', y') = H.insert (Entry (x', y') (steps + 1)) q
-            shouldVisit (x', y') = x' >= 0 && x' <= 70 && y' >= 0 && y' <= 70 &&
-                not ((x', y') `S.member` bytes) && not ((x', y') `S.member` visited)
+            avoid' = S.insert (x, y) avoid -- Avoid both corrupt bytes and already visited positions.
+            queue' = foldl' enqueue tailQueue $ filter shouldVisit neighbors
+            neighbors = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+            shouldVisit (x', y') = x' >= 0 && x' <= 70 && y' >= 0 && y' <= 70 && (x', y') `S.notMember` avoid
+            enqueue q p = H.insert (QueueEntry p (steps + 1)) q
 
+-- Get the position of the first corrupt byte that blocks the path from 0,0 to 70,70.
 firstBlockingByte :: [Pos] -> Pos
-firstBlockingByte input = input !! bisect 0 (length input - 1)
+firstBlockingByte input = input !! bisect 0 (length input)
   where
     bisect lo hi
-        | lo + 1 == hi = lo
+        | lo + 1 == hi = lo -- Return lo instead of hi so it can be used as a zero-based index.
         | otherwise = let mid = (lo + hi) `div` 2
-            in if possible mid then bisect mid hi else bisect lo mid
-    possible = isJust . shortestPath . flip take input
+            in if isBlocked mid then bisect lo mid else bisect mid hi
+    isBlocked = isNothing . shortestPath . flip take input
 
 parseInput :: String -> [Pos]
 parseInput = map parseLine . lines
